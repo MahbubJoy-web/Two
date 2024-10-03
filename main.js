d0b9dca1844046dcd77b0e5735cb5e4842e1ba6e
MahbubJoy-web <mahbubjoy.web@gmail.com>
2024-10-04 04:37:22 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var Joy = document.querySelector('h1')
+
+for( let himu = 2 ; himu<22 ; himu++){
+   Joy.innerHTML += 22 + ' x ' + himu + ' = ' + 22*himu + '<br>'
+   console.log(Joy.innerHTML
+   );
+   
+    
+}
+
+// ==== 2 x 1 = 2
